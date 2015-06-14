542835494ab7dad35a47e4d0e0d20a2c6eb60ac4
Thanabhat Koomsubha <thanabhat@gmail.com>
2015-06-14 14:20:46 +0700

    refactor

--- src/HMC/Main.java
+++ src/HMC/Main.java
@@ -30,67 +30,7 @@ public class Main {
 //		 System.out.println();
 		 
 		 //normalize numeric data
-		 Double[] maxNumericParameter = new Double[dataTrain.attributes.size()];
-		 Double[] minNumericParameter = new Double[dataTrain.attributes.size()];
-		 java.util.Arrays.fill(maxNumericParameter, -1000000.0);
-		 java.util.Arrays.fill(minNumericParameter, 1000000.0);
-		 
-		for (int i = 0; i < dataTrain.dataEntries.size(); i++) {
-			 for(int j=0;j<maxNumericParameter.length;j++){
-				 Object parameter = dataTrain.dataEntries.get(i).parameters.get(j);
-				 if(parameter instanceof NumericParameter){
-					 Double value = ((NumericParameter) parameter).getValue();
-					 if(value!=null){
-						 maxNumericParameter[j] = maxNumericParameter[j]>value?maxNumericParameter[j]:value;
-						 minNumericParameter[j] = minNumericParameter[j]<value?minNumericParameter[j]:value;
-					 }
-				 }
-			 }
-		}
-		for (int i = 0; i < dataTest.dataEntries.size(); i++) {
-			 for(int j=0;j<maxNumericParameter.length;j++){
-				 Object parameter = dataTest.dataEntries.get(i).parameters.get(j);
-				 if(parameter instanceof NumericParameter){
-					 Double value = ((NumericParameter) parameter).getValue();
-					 if(value!=null){
-						 maxNumericParameter[j] = maxNumericParameter[j]>value?maxNumericParameter[j]:value;
-						 minNumericParameter[j] = minNumericParameter[j]<value?minNumericParameter[j]:value;
-					 }
-				 }
-			 }
-		}
-		
-		//normalize
-		for (int i = 0; i < dataTrain.dataEntries.size(); i++) {
-			 for(int j=0;j<maxNumericParameter.length;j++){
-				 Object parameter = dataTrain.dataEntries.get(i).parameters.get(j);
-				 if(parameter instanceof NumericParameter){
-					 Double value = ((NumericParameter) parameter).getValue();
-					 if(value!=null){
-						 if(maxNumericParameter[j]!=minNumericParameter[j]){
-							 ((NumericParameter) parameter).setValue((value-minNumericParameter[j])/(maxNumericParameter[j]-minNumericParameter[j]));
-						 }else{
-							 ((NumericParameter) parameter).setValue(0.5);
-						 }
-					 }
-				 }
-			 }
-		}
-		for (int i = 0; i < dataTest.dataEntries.size(); i++) {
-			 for(int j=0;j<maxNumericParameter.length;j++){
-				 Object parameter = dataTest.dataEntries.get(i).parameters.get(j);
-				 if(parameter instanceof NumericParameter){
-					 Double value = ((NumericParameter) parameter).getValue();
-					 if(value!=null){
-						 if(maxNumericParameter[j]!=minNumericParameter[j]){
-							 ((NumericParameter) parameter).setValue((value-minNumericParameter[j])/(maxNumericParameter[j]-minNumericParameter[j]));
-						 }else{
-							 ((NumericParameter) parameter).setValue(0.5);
-						 }
-					 }
-				 }
-			 }
-		}
+		Utility.numericalNormalizer(new HMCDataContainer[]{dataTrain,dataTest});
 
 		 
 //		dataTrain.hierarchical.printHierarchical();
--- src/HMC/Utility.java
+++ src/HMC/Utility.java
@@ -7,6 +7,7 @@ import HMC.Container.Attribute.Attribute;
 import HMC.Container.Attribute.Hierarchical;
 import HMC.Container.Attribute.HierarchicalNode;
 import HMC.Container.Data.DataEntry;
+import HMC.Container.Data.NumericParameter;
 
 public class Utility {
 	public static boolean isMandatoryLeafNode(Hierarchical hierarchical, ArrayList<DataEntry> dataEntries){
@@ -35,6 +36,46 @@ public class Utility {
 						node.addPredictedMember(dataEntry);
 					}
 				}
+			}
+		}
+	}
+	
+	public static void numericalNormalizer(HMCDataContainer[] data){
+		Double[] maxNumericParameter = new Double[data[0].attributes.size()];
+		Double[] minNumericParameter = new Double[data[0].attributes.size()];
+		java.util.Arrays.fill(maxNumericParameter, -1000000.0);
+		java.util.Arrays.fill(minNumericParameter, 1000000.0);
+		 
+		for(int i_data=0;i_data<data.length;i_data++){
+			for (int i = 0; i < data[i_data].dataEntries.size(); i++) {
+				 for(int j=0;j<maxNumericParameter.length;j++){
+					 Object parameter = data[i_data].dataEntries.get(i).parameters.get(j);
+					 if(parameter instanceof NumericParameter){
+						 Double value = ((NumericParameter) parameter).getValue();
+						 if(value!=null){
+							 maxNumericParameter[j] = maxNumericParameter[j]>value?maxNumericParameter[j]:value;
+							 minNumericParameter[j] = minNumericParameter[j]<value?minNumericParameter[j]:value;
+						 }
+					 }
+				 }
+			}
+		}
+
+		for(int i_data=0;i_data<data.length;i_data++){
+			for (int i = 0; i < data[i_data].dataEntries.size(); i++) {
+				 for(int j=0;j<maxNumericParameter.length;j++){
+					 Object parameter = data[i_data].dataEntries.get(i).parameters.get(j);
+					 if(parameter instanceof NumericParameter){
+						 Double value = ((NumericParameter) parameter).getValue();
+						 if(value!=null){
+							 if(maxNumericParameter[j]!=minNumericParameter[j]){
+								 ((NumericParameter) parameter).setValue((value-minNumericParameter[j])/(maxNumericParameter[j]-minNumericParameter[j]));
+							 }else{
+								 ((NumericParameter) parameter).setValue(0.5);
+							 }
+						 }
+					 }
+				 }
 			}
 		}
 	}

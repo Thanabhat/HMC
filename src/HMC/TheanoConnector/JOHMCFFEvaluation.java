6fa128772b12882559da9a976f77c03e37140cef
Thanabhat Koomsubha <thanabhat@gmail.com>
2015-06-15 03:46:11 +0700

    add johmcff evaluation

---
+++ src/HMC/TheanoConnector/JOHMCFFEvaluation.java
@@ -0,0 +1,28 @@
+package HMC.TheanoConnector;
+
+import java.io.IOException;
+
+import HMC.Utility;
+import HMC.Container.HMCDataContainer;
+import HMC.Evaluator.ELb;
+import HMC.Reader.ARFFReader;
+
+public class JOHMCFFEvaluation {
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		final String dataset = "church_FUN";
+
+		HMCDataContainer dataTest = ARFFReader.readFile("datasets/datasets_FUN/"+dataset+"/"+dataset+".test.arff");
+		HMCDataContainer joPrediction = ARFFReader.readFile("datasets/datasets_FUN/"+dataset+"/"+dataset+".test.pred.johmcff");
+		dataTest.hierarchical.clearAllPredictedMember();
+		Utility.assignClusPredictionToContainer(dataTest, joPrediction, 0.5);
+//		HMC.Evaluator.Utility.printResult(dataTest.dataEntries);
+		HMC.Evaluator.Utility.PrepareParameter(dataTest.hierarchical);
+		ELb.Evaluate(dataTest.hierarchical, dataTest.dataEntries);
+	}
+
+}
